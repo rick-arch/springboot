e02c0efc4badb5e4a1a18b95951aec71056491c8
Administrator <2405242004@qq.com>
2020-10-14 16:00:07 +0800

    lua 修改

--- src/main/java/com/example/springboot/aspect/LimitAspect.java
+++ src/main/java/com/example/springboot/aspect/LimitAspect.java
@@ -88,21 +88,20 @@ public final class LimitAspect {
      * @return lua脚本
      */
     public String buildLuaScript() {
-        StringBuilder lua = new StringBuilder();
-        lua.append("local c");
-        lua.append("\nc = redis.call('get',KEYS[1])");
-        // 调用不超过最大值，则直接返回
-        lua.append("\nif c and tonumber(c) > tonumber(ARGV[1]) then");
-        lua.append("\nreturn c;");
-        lua.append("\nend");
-        // 执行计算器自加
-        lua.append("\nc = redis.call('incr',KEYS[1])");
-        lua.append("\nif tonumber(c) == 1 then");
-        // 从第一次调用开始限流，设置对应键值的过期
-        lua.append("\nredis.call('expire',KEYS[1],ARGV[2])");
-        lua.append("\nend");
-        lua.append("\nreturn c;");
-        return lua.toString();
+        String lua = "local c" +
+                "\nc = redis.call('get',KEYS[1])" +
+                // 调用不超过最大值，则直接返回
+                "\nif c and tonumber(c) > tonumber(ARGV[1]) then" +
+                "\nreturn c;" +
+                "\nend" +
+                // 执行计算器自加
+                "\nc = redis.call('incr',KEYS[1])" +
+                "\nif tonumber(c) == 1 then" +
+                // 从第一次调用开始限流，设置对应键值的过期
+                "\nredis.call('expire',KEYS[1],ARGV[2])" +
+                "\nend" +
+                "\nreturn c;";
+        return lua;
     }
 
     public String getIpAddress() {
