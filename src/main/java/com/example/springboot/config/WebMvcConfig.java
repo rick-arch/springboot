5cd1bbe475b48e64a5866f0e7e54e5c7e3965835
Administrator <2405242004@qq.com>
2020-11-28 15:54:50 +0800

    rick

--- src/main/java/com/example/springboot/config/WebMvcConfig.java
+++ src/main/java/com/example/springboot/config/WebMvcConfig.java
@@ -1,22 +1,22 @@
-package com.example.springboot.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * 跨域配置
- */
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .maxAge(3600)
-                .allowCredentials(true);
-    }
-}
+// package com.example.springboot.config;
+//
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+// /**
+//  * 跨域配置
+//  */
+// @Configuration
+// public class WebMvcConfig implements WebMvcConfigurer {
+//
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/**")
+//                 .allowedOrigins("*")
+//                 .allowedMethods("*")
+//                 .allowedHeaders("*")
+//                 .maxAge(3600)
+//                 .allowCredentials(true);
+//     }
+// }
